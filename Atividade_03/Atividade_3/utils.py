fea4286cf61fa0dfacd1a7ed391163c8cbf36ea0
Lucas Pereira Pesse <80925462+LPesse@users.noreply.github.com>
2021-06-28 22:16:02 -0300

    Atualização na geração do mapa
    Objetos no mapa não se sobrepõem mais

--- Atividade_03/Atividade_3/utils.py
+++ Atividade_03/Atividade_3/utils.py
@@ -30,14 +30,32 @@ def make_sand(grid):
     orientation = int(random(2))
     spread_point_x = int(random(17))
     spread_point_y = int(random(17))
+    generate = True
     if orientation == 0: #Horizontal Sand Rectangle
+        while generate:
+            generate = False
+            for i in range(3):
+                for j in range(2):
+                    if grid[spread_point_x + i][spread_point_y + j].wall == True:
+                        spread_point_x = int(random(17))
+                        spread_point_y = int(random(17))
+                        generate = True
         for i in range(3):
             for j in range(2):
                 if not grid[spread_point_x + i][spread_point_y + j].wall:
                     grid[spread_point_x + i][spread_point_y + j].sand = True
                     grid[spread_point_x + i][spread_point_y + j].f = 1
                     grid[spread_point_x + i][spread_point_y + j]
+                    
     else: #Vertical Sand Rectangle
+        while generate:
+            generate = False
+            for i in range(2):
+                for j in range(3):
+                    if grid[spread_point_x + i][spread_point_y + j].wall == True:
+                        spread_point_x = int(random(17))
+                        spread_point_y = int(random(17))
+                        generate = True
         for i in range(2):
             for j in range(3):
                 if not grid[spread_point_x + i][spread_point_y + j].wall:
@@ -49,14 +67,32 @@ def make_mud(grid):
     orientation = int(random(2))
     spread_point_x = int(random(13))
     spread_point_y = int(random(13))
+    generate = True
     if orientation == 0: #Horizontal Mud Rectangle
+        while generate:
+            generate = False
+            for i in range(7):
+                for j in range(4):
+                    if grid[spread_point_x + i][spread_point_y + j].wall == True or grid[spread_point_x + i][spread_point_y + j].sand == True:
+                        spread_point_x = int(random(13))
+                        spread_point_y = int(random(13))
+                        generate = True
         for i in range(7):
             for j in range(4):
                 if not grid[spread_point_x + i][spread_point_y + j].wall and not grid[spread_point_x + i][spread_point_y + j].sand:
                     grid[spread_point_x + i][spread_point_y + j].mud = True
                     grid[spread_point_x + i][spread_point_y + j].f = 5
                     grid[spread_point_x + i][spread_point_y + j]
+                    
     else: #Vertical Mud Rectangle
+        while generate:
+            generate = False
+            for i in range(4):
+                for j in range(7):
+                    if grid[spread_point_x + i][spread_point_y + j].wall == True or grid[spread_point_x + i][spread_point_y + j].sand == True:
+                        spread_point_x = int(random(13))
+                        spread_point_y = int(random(13))
+                        generate = True
         for i in range(4):
             for j in range(7):
                 if not grid[spread_point_x + i][spread_point_y + j].wall:
@@ -67,6 +103,15 @@ def make_mud(grid):
 def make_water(grid):
     spread_point_x = int(random(15))
     spread_point_y = int(random(15))
+    generate = True
+    while generate:
+        generate = False
+        for i in range(5):
+            for j in range(5):
+                if grid[spread_point_x + i][spread_point_y + j].wall == True or grid[spread_point_x + i][spread_point_y + j].sand == True or grid[spread_point_x + i][spread_point_y + j].mud == True:
+                    spread_point_x = int(random(15))
+                    spread_point_y = int(random(15))
+                    generate = True
     for i in range(5):
         for j in range(5):
             if not grid[spread_point_x + i][spread_point_y + j].wall and not grid[spread_point_x + i][spread_point_y + j].sand and not grid[spread_point_x + i][spread_point_y + j].mud:
